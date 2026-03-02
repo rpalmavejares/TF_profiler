c1a1e76460beaaf6dc18ccb1f677f2781ff629c6
rpalmavejares <rpalmavejares@gmail.com>
2026-03-02 05:09:38 -0300

    rework 8

--- src/parse_results.py
+++ src/parse_results.py
@@ -1,69 +1,84 @@
 import sys
+import argparse
 
-fasta_operon_file = open(sys.argv[1],"r")
-mast_alignment_file = open(sys.argv[2],"r")
+def main ():
 
+    parser = argparse.ArgumentParser(
+        description="",
+        usage="")
 
-operon_cds_dict=dict()
-operon_contig_dict=dict()
+    parser.add_argument("--prr_fasta",metavar="",help="")    
+    parser.add_argument("--motif_alignment",metavar="",help="")
+    parser.add_argument("--sample_id",metavar="",help="")
+    parser.add_argument("--motif_db",metavar="",help="")
+    parser.add_argument("--output",metavar="",help="")
 
+    if len(sys.argv) == 1:
+        print("\n")
+        parser.print_help()
+        sys.exit(1)
 
+    args = parser.parse_args()    
 
+    operon_cds_dict=dict()
+    operon_contig_dict=dict()
+    
+    with open(args.prr_fasta) as prr_fasta_file:
+        for operons in prr_fasta_file:
+            if(">" in operons):
+                aux=operons.split(" ")
+                operon_name=aux[0].replace(">","")
+                contig_name=aux[1].replace("Replicon=","")
+                genes=aux[5].replace("Genes=","")
+                operon_contig_dict[operon_name]=contig_name
+                operon_cds_dict[operon_name]=genes.replace("\n","")
 
-for operons in fasta_operon_file:
+    TF_flag=False
+    Best=False
+    Register=False
 
-	if(">" in operons):
-		aux=operons.split(" ")
-		operon_name=aux[0].replace(">","")
-		contig_name=aux[1].replace("Replicon=","")
-		genes=aux[5].replace("Genes=","")
-		operon_contig_dict[operon_name]=contig_name
-		operon_cds_dict[operon_name]=genes.replace("\n","")
-		#print(operon_name,contig_name,genes)
+    all_data=[]
 
-TF_flag=False
-Best=False
-Register=False
+    motif_db_dir=(args.motif_db).rstrip("/")
+   
+    outfile = open(args.output,"a")
+ 
+    with open(args.motif_alignment) as motif_alignment_file:
+        for mast in motif_alignment_file:
+            if("# mast -norc -nostatus -hit_list -best" in mast):
+                if(len(all_data)>0):
+                    TFBS_taxa=mast.split(" ")
+                    TFBS_taxa=TFBS_taxa[6].replace(motif_db_dir+"/","")
+                    for data in all_data:
+                        outfile.write(TFBS_taxa+","+data+"\n")	
+                    all_data=[] 
+                continue
+            if("# Best single (non-overlapping)" in mast):
+                Best=True
+                continue
+            if("# sequence_name" in mast):
+                Register=True
+                continue
+            if(Best and Register):
+                aux=mast.split(" ")
+                genes=operon_cds_dict[aux[0]]
+                mast_stats=[]
+                for values in aux:
+                    if(values!=""):
+                        mast_stats.append(values.replace("\n",""))
+                if("," in genes):
+                    each_gene=genes.split(",")
+                    for each in each_gene:
+                        all_data.append(aux[0]+","+each+","+",".join(mast_stats[3:10]))
+                else:
+                    all_data.append(aux[0]+","+genes+","+",".join(mast_stats[3:10]))
 
-all_data=[]
+        if(len(all_data)>0):
+            for data in all_data:
+                outfile.write(TFBS_taxa+","+data,"\n")
 
-for mast in mast_alignment_file:
-	if("# mast -norc -nostatus -hit_list -best Regprecise_TF_DB/" in mast):
-		if(len(all_data)>0):
-			TFBS_taxa=mast.split(" ")
-			TFBS_taxa=TFBS_taxa[6].replace("Regprecise_TF_DB/","")
-			#print(TFBS_taxa,all_data)
-			for data in all_data:
-				print(TFBS_taxa+","+data)	
-			all_data=[] 
-		continue
-	if("# Best single (non-overlapping)" in mast):
-		Best=True
-		continue
-	if("# sequence_name" in mast):
-		Register=True
-		continue
-	if(Best and Register):
-		aux=mast.split(" ")
-		genes=operon_cds_dict[aux[0]]
-		mast_stats=[]
-		for values in aux:
-			if(values!=""):
-				mast_stats.append(values.replace("\n",""))
-		#print(mast_stats)	
-		if("," in genes):
-			each_gene=genes.split(",")
-			for each in each_gene:
-				#print(aux[0]+","+each+","+",".join(mast_stats[1:10]))
-				all_data.append(aux[0]+","+each+","+",".join(mast_stats[3:10]))
-		else:
-			#print(aux[0]+","+genes+","+",".join(mast_stats[1:10]))
-			all_data.append(aux[0]+","+genes+","+",".join(mast_stats[3:10]))
-
-if(len(all_data)>0):
-	for data in all_data:
-		print(TFBS_taxa+","+data)
-
+if __name__ == "__main__":
+    main()
 
 
 
